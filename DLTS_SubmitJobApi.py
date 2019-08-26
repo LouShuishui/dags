c18774f3de09575c6160f97ff76239a160de5367
wendy1130 <wendyou1130@gmail.com>
2019-08-26 14:36:40 -0700

    Create DLTS_SubmitJobApi.py

---
+++ DLTS_SubmitJobApi.py
@@ -0,0 +1,30 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime, timedelta
+
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2015, 6, 1),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+dag = DAG('DLTASubmitJobApi', default_args=default_args, schedule_interval=timedelta(days=1))
+
+t1 = BashOperator(
+    task_id='print_date',
+    command='python dltsPostJobApi',
+    dag=dag)
+
+t2 = BashOperator(
+    task_id='sleep',
+    bash_command='sleep 5',
+    retries=3,
+    dag=dag)
+
+t2.set_upstream(t1)
