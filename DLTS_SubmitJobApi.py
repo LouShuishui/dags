70fdbe4ec4da0d3c8b800c51b4bc1513305221e4
wendy1130 <wendyou1130@gmail.com>
2019-08-29 11:33:36 -0700

    Update DLTS_SubmitJobApi.py

--- DLTS_SubmitJobApi.py
+++ DLTS_SubmitJobApi.py
@@ -1,8 +1,10 @@
 import airflow
+import requests
+import json
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
 from datetime import datetime, timedelta
-
 
 default_args = {
     'owner': 'airflow',
@@ -15,12 +17,52 @@ default_args = {
     'retry_delay': timedelta(minutes=1),
 }
 
-dag = DAG('DLTASubmitJobApi', default_args=default_args, schedule_interval=timedelta(minutes=60))
+dag = DAG('submit_dlts_job', default_args=default_args, schedule_interval=timedelta(minutes=60))
 
-t1 = BashOperator(
-    task_id='postDLTSJob',
-    bash_command='python /opt/bitnami/airflow/dags/dltsPostJobApi.py',
+def post_dlts_job():
+	submit_url = "http://dltshub-aether.westus2.cloudapp.azure.com/api/dlws/postJob?cluster=Azure-EastUS-V100-LowPriority&Team=ads&Email=weouyan@microsoft.com&Key=871c0ee3"
+
+	jobParams = {
+		"cmd" : "sleep infinity",
+		"dataPath" : "",
+		"enabledatapath" : True,
+		"enablejobpath" : True,
+		"enableworkpath" : True,
+		"env" : "[ ]",
+		"gpuType" : "V100",
+		"hostNetwork" : False,
+		"image": "indexserveregistry.azurecr.io/zhrui/horizon",
+		"is_interactive": True,
+		"interactivePorts" : "[40001]",
+		"ipython" : True,
+		"isParent" : 1,
+		"isPrivileged" : False,
+		"jobName" : "TestAPISubmission",
+		"jobType" : "training",
+		"jobtrainingtype" : "RegularJob",
+		"preemptionAllowed" : False,
+		"resourcegpu": 0,
+		"runningasroot": True,
+		"ssh" : True,
+		"tensorboard" : True,
+		"userName" : "weouyan@microsoft.com",
+		"vcName" : "ads",
+		"workPath" : "weouyan"
+	}
+	payload = {}
+	payload["Json"] = json.dumps(jobParams)
+
+	r = requests.post(
+		url=submit_url,
+		data=payload
+	)
+
+t1 = PythonOperator(
+    task_id='post_request',
+    python_callable=post_dlts_job,
     dag=dag)
+
+
 
 t2 = BashOperator(
     task_id='sleep',
